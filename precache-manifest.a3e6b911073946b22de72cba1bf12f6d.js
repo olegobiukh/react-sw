cd18045fe8d551703dfd453eafdccede71fbe4cc
obiukh19 <oleg.obiukh@gmail.com>
2019-03-07 21:24:38 +0200

    Updates

---
+++ precache-manifest.a3e6b911073946b22de72cba1bf12f6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-sw/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "febe90fbedb491cb3aa2",
+    "url": "/react-sw/static/js/runtime~main.5400a88b.js"
+  },
+  {
+    "revision": "193fcfc20c67084eedd8",
+    "url": "/react-sw/static/js/main.7176e09b.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/react-sw/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "193fcfc20c67084eedd8",
+    "url": "/react-sw/static/css/main.6c2579e7.chunk.css"
+  },
+  {
+    "revision": "51c455dff69b18213dc9b9df76173928",
+    "url": "/react-sw/index.html"
+  }
+];
